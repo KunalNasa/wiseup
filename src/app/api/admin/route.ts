946e5eb1c703fed761f7b14510b4a1f47aa75739
KunalNasa <kunalnasa.dev@gmail.com>
2024-12-03 18:10:55 +0530

    remove linting comments

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import prisma from "@/lib/prisma";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
--- src/app/api/subscriptionHistory/route.ts
+++ src/app/api/subscriptionHistory/route.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
