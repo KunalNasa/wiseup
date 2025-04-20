b5e136f849f7b0579f678cb723cfd7d9fae33b5b
KunalNasa <kunalnasa.dev@gmail.com>
2025-04-21 04:17:40 +0530

    subscription page done, only resposiveness and caching for user profile left

--- src/app/(authenticated)/subscribe/page.tsx
+++ src/app/(authenticated)/subscribe/page.tsx
@@ -11,7 +11,7 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 const page = () => {
   const amount = 1.00
   return (
-    <main className="w-11/12 mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+    <main className="w-11/12 mx-auto overflow-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
     <div className="mb-10">
       <h1 className="text-4xl font-extrabold mb-2">WiseUp</h1>
       <h2 className="text-2xl">
